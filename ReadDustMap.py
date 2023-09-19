f556e910c65c7f3c17276ed04e68308eca1f955a
KatiaMich <126151171+KatiaMich@users.noreply.github.com>
2023-09-19 18:55:08 +0200

    Update ReadDustMap.py

--- ReadDustMap.py
+++ ReadDustMap.py
@@ -1,28 +1,28 @@
 import numpy as np
 import pandas as pd
 
-def averagedExtinction(ListOfVects,co,interpolated_dustMap):
+def averagedExtinction(ListOfVects,co,interpolated_dustMap): #gives a list of extinction between the target location and all stars in the catalogue
     numberPoints = 300 
     dl = np.linspace(0,1,numberPoints)**2 #quadratic spacing of the sample points
     VectOfSight = ListOfVects- co[None,:] #co are OriginPoints
     norms = np.linalg.norm(VectOfSight,axis=1)
     extinctions = np.zeros((numberPoints,len(VectOfSight)),dtype=float)
-    averagedExtinctions = np.zeros(len(VectOfSight),dtype=float)
+    differential_averagedExtinctions = np.zeros(len(VectOfSight),dtype=float)
     for k in range(numberPoints):
-        LineOfSight = dl[k]*VectOfSight + co[None,:] 
-        extinctions[k] = interpolated_dustMap(LineOfSight) #extinctions[k] has as many elements as stars in the catalog
-        averagedExtinctions += extinctions[k]*dl[k]/np.sum(dl) 
-    return averagedExtinctions,norms
+        point = dl[k]*VectOfSight + co[None,:] 
+        differential_extinctions[k] = interpolated_dustMap(point) #extinctions[k] has as many elements as stars in the catalog
+        differential_averagedExtinctions += differential_extinctions[k]*dl[k]/np.sum(dl) 
+    averaged_extinctions = np.array(differential_averagedExtinctions*norms, dtype=floats)
+    return differential_averagedExtinctions,norms
 
-def ListOfVect(pdtable): #gives the coordinates of the star in the frame of the dustmap, which is galactic, centered at the sun.
-    x,y,z = pdtable['x(pc)'], pdtable['y(pc)'], pdtable['z(pc)']
+def ListOfVect(catalogue_filename): #gives the coordinates of the star in the frame of the dustmap, which is galactic, centered at the sun.
+    catalogue = pd.read_pickle(catalogue_filename)
+    x,y,z = catalogue['x(pc)'], catalogue['y(pc)'], catalogue['z(pc)']
     x,y,z = np.array(x),np.array(y),np.array(z) 
     Coord = np.array([x,y,z])
     return np.transpose(Coord)
 
-def getAv(filename,co,interpolated_dustMap): 
-    pdtable = pd.read_pickle(filename)
-    ListOfVects = ListOfVect(pdtable)
-    dA_dD, norms = averagedExtinction(ListOfVects,co,interpolated_dustMap)
-    Av = np.array(dA_dD * norms,dtype=float)
+def getAv(catalogue_filename,co,interpolated_dustMap): 
+    ListOfVects = ListOfVect(catalogue_filename)
+    Av, norms = averagedExtinction(ListOfVects,co,interpolated_dustMap)
     return Av , norms 
